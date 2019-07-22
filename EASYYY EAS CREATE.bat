cb9149cd19bc5a0800f1e66e017f110d61fc2989
InsertNameHere42 <mayer.brigan@gmail.com>
2019-07-22 12:34:51 -0700

    Update EASYYY EAS CREATE.bat

--- EASYYY EAS CREATE.bat
+++ EASYYY EAS CREATE.bat
@@ -10,23 +10,24 @@ easencode.py -v
 echo Read the README file before cont.
 echo ============== - + - ==============
 echo Audio In?
-set /p "ain=y/n "
+set /p "ain=y/n: "
 if "%ain%"=="n" goto NoAudioIn
 if "%ain%"=="y" goto AudioIn
+if "%ain%"=="N" goto NoAudioIn
+if "%ain%"=="Y" goto AudioIn
 goto top
 
 
 
 :NoAudioIn
+set /p "rrr=Set Originator (WXR, EAS): "
 set /p "eee=Set 3 Digit Event Code: "
-set /p "lll=Set SAME Locations (Space Seperated): "
-set /p "ddd=Set Duration: "
+set /p "lll=Set SAME Locations (Space Seperated, National Disabled): "
 set /p "ooo=Set Output Name: "
 
 if "%eee%"=="" set "eee=DMO"
 set "ttt=now"
-set "rrr=wxr"
-set "bbb=TSTALRT"
+set "bbb=TESTALRT"
 if "%ooo%"=="" set "ooo=Output"
 
 echo ================
@@ -35,16 +36,15 @@ echo ================
 goto spa
 
 :AudioIn
-set /p "eee=Set Event: "
-set /p "lll=Set SAME Locations (Space Seperated): "
-set /p "ddd=Set Duration: "
-set /p "aaa=Audio In Directory (MUST BE WAV): 
+set /p "rrr=Set Originator (WXR, EAS): "
+set /p "eee=Set 3 Digit Event Code: "
+set /p "lll=Set SAME Locations (Space Seperated, National Disabled): "
+set /p "aaa=Audio In Directory: 
 set /p "ooo=Set Output Name: "
 
 if "%eee%"=="" set "eee=DMO"
 set "ttt=now"
-set "rrr=wxr"
-set "bbb=TSTALRT"
+set "bbb=TESTALRT"
 if "%ooo%"=="" set "ooo=Output"
 
 echo ================
@@ -54,14 +54,19 @@ goto spa
 
 :spa
 set /p Play="Play Audio? (y/n): "
+echo.
+set /p mrr="Play using default player? (y/n): "
 
 if %play%==y goto cont
+if %play%==Y goto cont
 
 pause
 cls
 goto top
 
 :cont
+if %mrr%==y goto skrp
+if %mrr%==Y goto skrp
 echo Set Sound = CreateObject("WMPlayer.OCX.7") > %cd%\sound.vbs
 echo Sound.URL = "%cd%\%ooo%.wav" >> %cd%\sound.vbs
 echo Sound.Controls.play >> %cd%\sound.vbs
@@ -70,6 +75,13 @@ echo wscript.sleep 100 >> %cd%\sound.vbs
 echo loop >> %cd%\sound.vbs
 echo wscript.sleep (int(Sound.currentmedia.duration)+1)*1000 >> %cd%\sound.vbs
 start %cd%\sound.vbs
+pause
+del %cd%\sound.vbs
+cls
+goto top
+
+:skrp
+start %ooo%.wav
 pause
 cls
 goto top
