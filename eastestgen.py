deb992590d5c8e9336262dbe8fab63618cce785d
InsertNameHere42 <mayer.brigan@gmail.com>
2019-07-22 12:37:20 -0700

    Update eastestgen.py

--- eastestgen.py
+++ eastestgen.py
@@ -3,38 +3,34 @@ from afsk import *
 eastestgen_core_version = "0.0.1 BETA"
 
 def generateEASpcmData(org, event, fips, eventDuration, timestamp, stationId, sampRate, sampWidth, 
-	                peakLevel, numCh, msgaudio=None, customMsg=None):
-    "Put together info to generate an EAS message"
+	                peakLevel, numCh, msgaudio=None):
+	"Put together info to generate an EAS message"
 
-    markF = 2083.3
-    spaceF = 1562.5
-    bitrate = 520.5
-    pcm_data = ''
+	markF = 2083.3
+	spaceF = 1562.5
+	bitrate = 520.5
+	pcm_data = ''
 
-    preamble = '\xab' * 16
-    if customMsg is not None:
-	message = 'ZCZC-' + customMsg
-    else:
+	preamble = '\xab' * 16
 	message = 'ZCZC-{0}-{1}-{2}+{3}-{4}-{5: <8}-'.format(org, event, "-".join(fips[0:31]), 
-		    eventDuration, timestamp, stationId[0:8])
-    endOfMessage = 'NNNN'
-    header = generateAFSKpcmData(markF, spaceF, bitrate, sampRate, sampWidth, peakLevel,
-	                 numCh, preamble + message.upper())
-    eom = generateAFSKpcmData(markF, spaceF, bitrate, sampRate, sampWidth, peakLevel,
-	                 numCh, preamble + endOfMessage)
-    silence = generateSimplePCMToneData(10000, 10000, sampRate, 1, sampWidth, -94, numCh)
-    
-    pcm_data = silence + silence
+			eventDuration, timestamp, stationId[0:8])
+	endOfMessage = 'NNNN'
+	header = generateAFSKpcmData(markF, spaceF, bitrate, sampRate, sampWidth, peakLevel,
+					numCh, preamble + message.upper())
+	eom = generateAFSKpcmData(markF, spaceF, bitrate, sampRate, sampWidth, peakLevel,
+					numCh, preamble + endOfMessage)
+	silence = generateSimplePCMToneData(10000, 10000, sampRate, 1, sampWidth, -94, numCh)
 
-    for i in range(3):
-	pcm_data = pcm_data + header + silence
-    if msgaudio is not None:
-	attn_tones = generateDualTonePCMData(853, 960, sampRate, 8, sampWidth, peakLevel, numCh)
-	pcm_data = pcm_data + silence + attn_tones + silence + msgaudio + silence
-    for i in range(3):
-	pcm_data = pcm_data + eom + silence
+	pcm_data = silence + silence
 
-    return pcm_data
+	for i in range(3):
+		pcm_data = pcm_data + header + silence
+	if msgaudio is not None:
+		attn_tones = generateDualTonePCMData(853, 960, sampRate, 8, sampWidth, peakLevel, numCh)
+		pcm_data = pcm_data + silence + attn_tones + silence + msgaudio + silence + silence
+	for i in range(3):
+		pcm_data = pcm_data + silence + eom
+	return pcm_data
 
 if __name__ == "__main__":
     import wave, time
