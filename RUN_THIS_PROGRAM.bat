d572bbdcbe8697633155bad8bf80a74032e02cfb
A-c0rN <mayer.brigan@gmail.com>
2020-01-04 00:59:28 -0600

    Update RUN_THIS_PROGRAM.bat

--- RUN_THIS_PROGRAM.bat
+++ RUN_THIS_PROGRAM.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set "VOICE=Paul"
+set "LOCAL=000000"
 
 :top
 cls
@@ -9,36 +10,42 @@ title EAS Creator
 
 if not exist "%cd%\Output\" mkdir "%cd%\Output\"
 
-
-set /p "mmm=Set Originator (Default EAS): "
-set /p "eee=Set Event (Default DMO): "
-set /p "lll=Set SAME Locations (Space Seperated) (Default LOCAL): "
-set /p "ddd=Duration (Default 0015): "
-set /p "bbb=Set Callsign (Default WACN): "
+echo Thank you for using the EAS Genorator by A-c0rN and MazariFox.
+echo ==============================================================
+echo.
+echo Specify TTS voice and LOCAL by right-clicking this file, and
+echo selecting Edit. Should be Isolated.
+echo.
+echo Leave blank for Defaults.
+echo Defaults: EAS, DMO, LOCAL, 0015, SFTENCDR
+echo.
+set /p "mmm=Set Originator (Options: EAS, CIV, WXR): "
+set /p "eee=Set Event (Three letter event, EX: DMO): "
+set /p "lll=Set SAME Locations (Space Seperated): "
+set /p "ddd=Duration (Use NWR Specifications): "
+set /p "bbb=Set Callsign (Max 8 Characters): "
+echo.
+echo ==============================================================
 
 if "%mmm%"=="" set "mmm=EAS"
 if "%eee%"=="" set "eee=DMO"
-if "%eee%"=="RWT" (
-	if "%lll%"=="" set "lll=005000 006000 012000 013000 017000 020000 022000 028000 029000 040000 042000 047000 051000 005007 005143 040041"
-)
-if "%lll%"=="" set "lll=005007 005143 040041"
+if "%lll%"=="" set "lll=%LOCAL%"
 if "%ooo%"=="" set "ooo=ALERT"
 set "nnn=%ooo%-EOM"
 set "ttt=now"
 if "%ddd%"=="" set "ddd=0015"
-if "%bbb%"=="" set "bbb=WACN"
+if "%bbb%"=="" set "bbb=SFTENCDR"
 
-cls
 
-echo ENCODING ALERT NOW!
 echo.
+echo ENCODING ALERT NOW!
 echo.
 echo.
 easencode.py -o %mmm% -e %eee% -f %lll% -d %ddd% -t %ttt% -c "%bbb%" -x yes "%cd%\Output\%ooo%.wav"
 echo.
-echo.
-echo.
 easencode.py -o %mmm% -e %eee% -f %lll% -d %ddd% -t %ttt% -c "%bbb%" "%cd%\Output\%nnn%.wav"
+echo.
+echo ==============================================================
 
 (
 	echo Set Sound = CreateObject^("WMPlayer.OCX.7"^)
@@ -60,16 +67,16 @@ easencode.py -o %mmm% -e %eee% -f %lll% -d %ddd% -t %ttt% -c "%bbb%" "%cd%\Outpu
 	echo wscript.sleep ^(int^(Sound.currentmedia.duration^)+1^)*1000
 ) > "%cd%\Output\sound2.vbs"
 
-cls
-set /p "TEXT=Please enter any text you would like the application to say: "
-if "%TEXT%"=="RWT" set "TEXT=%cd%\Static\RWT.txt"
-cls
-
-echo Muting Audio...
-timeout 1 /nobreak >nul
-Static\nircmd.exe muteappvolume Music.UI.exe 1 >nul
-echo Done.
 echo.
+set /p "TEXT=Please enter any text you would like the application to say: "
+echo.
+echo ==============================================================
+echo.
+rem echo Muting Audio...
+rem timeout 1 /nobreak >nul
+rem Static\nircmd.exe muteappvolume Music.UI.exe 1 >nul
+rem echo Done.
+rem echo.
 
 timeout 1 /nobreak >nul
 
@@ -78,21 +85,24 @@ start /wait Output\sound.vbs "Static\autoclose.bat" >nul
 echo Done.
 echo.
 
-echo Sending Audio Message...
-Static\balcon.exe -n %VOICE% -s -8 -t "%TEXT%" >nul
-echo Done.
-echo.
+if not "%TEXT%"=="" (
+	echo Sending Audio Message...
+	Static\balcon.exe -n %VOICE% -s -8 -t "%TEXT%" >nul
+	echo Done.
+	echo.
+)
 
 echo Sending EOMs...
 start /wait Output\sound2.vbs "Static\autoclose.bat" >nul
 echo Done.
 echo.
 
-echo Unmuting Audio...
-timeout 1 /nobreak >nul
-Static\nircmd.exe muteappvolume Music.UI.exe 0 >nul
-echo Done.
-echo.
+rem echo Unmuting Audio...
+rem timeout 1 /nobreak >nul
+rem Static\nircmd.exe muteappvolume Music.UI.exe 0 >nul
+rem echo Done.
+rem echo.
+echo ==============================================================
 echo.
 pause
 goto top
